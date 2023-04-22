4c019f999096db4e470247fe2280df5c0c180280
gauthamkrishna1312 <gauthamkrishna1312@gmail.com>
2023-04-21 23:44:26 -0700

    Base template added

--- athma_2k23/core/views.py
+++ athma_2k23/core/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Hello Athma 2k23</h1>')
+    return render(request, 'index.html')
